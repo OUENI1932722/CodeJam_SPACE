b8641f03fc2f763ab81249c3c4699e3d7883adcd
Nicolas Ouellet-Duval <OueNi1932722@etu.cegepjonquiere.ca>
2020-02-08 17:04:20 -0500

    Carburant + Physique overhaul

--- CodeJam SPACE/Carburant.cs
+++ CodeJam SPACE/Carburant.cs
@@ -12,5 +12,35 @@ namespace CodeJam_SPACE
          Kerosene, Oxygène liquide, hydrogène liquide, hydrazine(diazane), aérozine 50, 1,1-diméthylhydrazine, peroxyde d'azote
          https://en.wikipedia.org/wiki/Liquid_rocket_propellant#Present_use
          */
+        private double poussee, densite, prix, quantite;
+
+        public enum TypeCarburant
+        {
+            Kerosène, Hydrogène, Méthane
+        }
+        public Carburant(TypeCarburant typeCarburant, double quantite)
+        {
+            switch(typeCarburant)
+            {
+                case TypeCarburant.Kerosène:
+                    poussee = 3510; //N*s/kg
+                    densite = 1.03; //kg/m^3
+                    prix: 0.73; //$/kg
+                    this.quantite = quantite;
+                    break;
+                case TypeCarburant.Hydrogène:
+                    poussee = 4462;
+                    densite = 0.32;
+                    prix: 5;
+                    this.quantite = quantite;
+                    break;
+                case TypeCarburant.Méthane:
+                    poussee = 3615;
+                    densite = 0.83;
+                    prix: 1.35;
+                    this.quantite = quantite;
+                    break;
+            }
+        }
     }
 }
--- CodeJam SPACE/Physique.cs
+++ CodeJam SPACE/Physique.cs
@@ -10,36 +10,33 @@ namespace CodeJam_SPACE
     {
         /*Décolage:
          *          https://tpefusee.wordpress.com/previsions-de-la-mission-les-parametre-de-lancement/ 
-         * Poids de la fusée: P = m * g, où P est le poids, m la masse(kg) et g, la gravité (m/s^2)
-         * 
-         * 
-         * Pour décoller, il faut que la force de poussée soit supèrieur au poids de la Terre.
-         * Formule de la vitesse d'éjection des gazs: ΔE = ΔEc + ΔEp + ΔU = Q + W
-         * Où ΔU = variation de l’energie interne
-         *
-         * ΔEc = energie cinétique = 1/2 mVf² – 1/2 mVi² ( ici = 0)
-         *
-         * ΔEp= energie potentielle= mghf – mghc (ici =0)
-         *
-         * Q= échange de chaleur ( ici =o car la fusée est isolée)
-         *
-         * W= travail de force de pression
-         * 
-         * Formule de la vitesse de satellisation: v = Rac. (2GM/R), où G est la constante gravitationelle, M la masse de la Terre(kg) et R, le rayon de la terre(m)
-         * 
          * 1 tonne = 1000kg
          */
 
-        private readonly int RAYON_TERRE = 6371000/*m*/;
-        private readonly double VITESSE_ECHAPPEMENT = 11.2/*km/s*/, MASSE_TERRE = (5.97342*Math.Pow(10,24))/*kg*/, CONSTANTE_GRAVITATIONNELLE = (6.67*Math.Pow(10,(-11)))/*(N*m^2)/kg^2*/, INTENSITE_GRAV_TERRE = 9.8/*N/kg*/, VITESSE_SATELLISATION = 7.9/*km/s*/, CONSTANTE_GAZ_PARFAITS = 8.314/*kPaL/molK*/;
-        private double poidsFusee, masseFusee, quantiteCarburant, pressionCombustion, pressionExterieure = 101.3/*kPa*/, masseMoleculaireCarburant/*initié avec chaque carburant, masse molaire*/, vitesseEjectionGaz, gammaGaz, pousseeFusee, debitMassiqueGaz;
+        private readonly double INTENSITE_GRAV_TERRE = 9.8/*N/kg*/, VITESSE_SATELLISATION = 7.9/*km/s*/;
+        private double poidsFusee, masseFusee, quantiteCarburant, pousseeFusee, debitMassiqueGaz, aireMoteur, densiteGaz, volumeGaz, accelerationFusee, velociteGaz = 0;
 
         double CalculerPoidsFusee()
         {
             poidsFusee = masseFusee * INTENSITE_GRAV_TERRE;
             return poidsFusee;
         }
-        double CalculerVitesseEjectionGaz()
+        double CalculerAcceleration()
+        {
+            accelerationFusee = (pousseeFusee - CalculerPoidsFusee()) / masseFusee;
+            return accelerationFusee;
+        }
+        double CalculerVelociteGaz()
+        {
+            velociteGaz = velociteGaz + CalculerAcceleration();
+            return velociteGaz; //m/s
+        }
+        double CalculerPerteMasse()
+        {
+            debitMassiqueGaz = densiteGaz * CalculerVelociteGaz() * aireMoteur;
+            return debitMassiqueGaz; //kg/s
+        }
+        /*double CalculerVitesseEjectionGaz()
         {
             //Ve = Rac((2k/k-1)(RTc/M)(1-Pe/Pc)^(k-1/k)) || Pe = 1bar, M = déclaré avec carburant, Pc = déclaré avec chambre combustion(moteur), Tc = déclaré avec carburant, R = 8.31, K = Gamma(voir https://fr.wikipedia.org/wiki/Indice_adiabatique) 
             vitesseEjectionGaz = Math.Sqrt(Math.Pow((((2*gammaGaz)/(gammaGaz-1))*((CONSTANTE_GAZ_PARFAITS - 1)/CONSTANTE_GAZ_PARFAITS)*(1 - (pressionExterieure/pressionCombustion))),((gammaGaz - 1)/gammaGaz)));
@@ -50,10 +47,10 @@ namespace CodeJam_SPACE
             //F = Dm * Ve|| F = poussée, Dm= débit massique de gaz éjecté(déclaré avec moteur)
             pousseeFusee = debitMassiqueGaz * vitesseEjectionGaz;
             return pousseeFusee;
-        }
+        }*/
         bool DecollageFusee()
         {
-            if (CalCulerVitesseDecollage() > CalculerPoidsFusee())
+            if (pousseeFusee > CalculerPoidsFusee())
                 return true;
             return false;
         }
